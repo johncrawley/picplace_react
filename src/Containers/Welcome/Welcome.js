39df2365dc6d1a18a61c900824606f619e5e9f3b
johncrawley <johncrawley1@gmail.com>
2020-03-30 20:11:19 +0100

    Updated and refactored loading of images to parse a pageable response which specifies if the current page is the last page. This negates the need to request a total number of photos and calculate last page status.

--- src/Containers/Welcome/Welcome.js
+++ src/Containers/Welcome/Welcome.js
@@ -1,97 +1,52 @@
 import React, { useEffect, useState } from 'react';
-import Axios, { axiosAuth } from '../../axios';
+import { axiosAuth } from '../../axios';
 
 
 const Welcome = (props) => {
     
-    
-
     useEffect(()=> {
-        
-        retrieveListOfPhotoIDs();
-        retrieveNumberOfPhotos();
+        retrievePageOfPhotoIDs();
     }, []);
 
     const [photoIds, setPhotoIds] = useState([]);
-    const [photoCount, setPhotoCount] = useState(0);
-    const [currentPage, setCurrentPage] = useState(1);
+    const [currentPage, setCurrentPage] = useState(0);
     const [isLastPage, setIsLastPage] = useState(false);
 
     const photosPerPage = 5;
 
-    const retrieveListOfPhotoIDs = () => {
-        const pageParams = getPageParams(currentPage);
-        axiosAuth.get('/photoIdsPage'+ pageParams)
+    const retrievePageOfPhotoIDs = () => {
+        axiosAuth.get('/photoIdsPage'+ getPageParams(currentPage))
         .then( response => {
             parsePhotoIdsFrom(response.data);
-            updateLastPageStatus(response.data);
-            updateCurrentPageNumber();
+            setIsLastPage(response.data.last);
+            setCurrentPage(pageNum => pageNum + 1);
             
         }).catch(error => {
             handleError("Retrieving Photo IDs", error);
         })
     }
 
+
     const getPageParams = (currentPage) => {
-        console.log("Current page: " + currentPage);
         return "?page=" + currentPage + "&size=" + photosPerPage + "&photoSize=thumbnail";
     }
 
+
     const parsePhotoIdsFrom = (responseData) => {
         let newIds = [];
-        responseData.photos.map( photo => newIds.push(photo.id));
+        responseData.content.map( photo => newIds.push(photo.id));
         setPhotoIds( existingIds => [...existingIds, ...newIds]);
     }
-
-    const updateLastPageStatus = (responseData) => {
-        setIsLastPage(responseData.last);
-    }
-
-    const updateCurrentPageNumber = () => {
-        setCurrentPage(prevPageNum => prevPageNum + 1);
-    }
-
-    const retrieveNumberOfPhotos = () => {
-
-        axiosAuth.get('/photocount')
-        .then( response => { setPhotoCount(response.data);}).catch(error => {handleError("Retrieving Photo count", error);} );
-    }
-
 
     const handleError = (label, error) => {
         console.log("Error (" + label + ") :" + error);
     }
 
-
-   // const preloadImage = (url) =>{
-    //    img1 =new Image();
-   //     img1.src=url;
-   // }
-
-    //preloadImage("http://localhost:8090/svc/photo");
-
     
-
-    const retrieveNextPage = () => {
-        console.log("retrieve next page!");
-
-        const pageParams = getPageParams(currentPage);
-        axiosAuth.get('/photoIdsPage' + pageParams)
-        .then( response => {
-            parsePhotoIdsFrom(response.data);
-            updateLastPageStatus(response.data);
-            updateCurrentPageNumber();
-        }).catch(error => {
-            
-        })
-        
-    }
-
     let photos = <h3> No Photos Found </h3>;
     let loadMoreButton = null;
-
-    if(!isLastPage){
-        loadMoreButton = <div><button onClick={retrieveNextPage}>Load More</button></div>
+    if(!isLastPage){        
+        loadMoreButton = <div><button onClick={retrievePageOfPhotoIDs}>Load More</button></div>
     }
 
 
@@ -101,9 +56,7 @@ const Welcome = (props) => {
     
     return(
     <div>
-
         <h1>welcome!</h1>
-        
         {photos}
         {loadMoreButton}
 
